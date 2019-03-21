cf1c6a685977eaa3f0a579557cce3bae01a4a742
jc428920 <izac.whalley-lombardo@my.jcu.edu.au>
2019-03-21 13:54:12 +1000

    added password checker code

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,8 @@
 """Izac Whalley-lombardo"""
+def main():
+    password = input("please enter password")
+    Password_lenght = 10
+    while password < Password_lenght :
+        password = input("Please enter a longer password:")
+
+
